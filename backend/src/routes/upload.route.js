32bf7e9271d10369151e49d0f4c411dae94bfc6b
Shahriyar-Rahim <rahimislam420.ri35@gmail.com>
2026-03-19 02:27:57 +0600

    picture uloader fix

--- backend/src/routes/upload.route.js
+++ backend/src/routes/upload.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import uploadController from "../controllers/upload.controller.js";
+import upload from "../middlewares/upload.js";
 
 const router = express.Router();
 
-router.post("/medicine/:id", uploadController.uploadMedicineImage);
-router.post("/shop/:id", uploadController.uploadShopImage);
+router.post("/medicine/:id", upload.single("image"), uploadController.uploadMedicineImage);
+router.post("/shop/:id", upload.single("image"), uploadController.uploadShopImage);
 router.delete("/delete", uploadController.deleteImage);
 
 export default router;
