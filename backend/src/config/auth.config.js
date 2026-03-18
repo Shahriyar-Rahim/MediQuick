5b33712d5b5756c6c940ca7f7d5b8af845df7b7d
Shahriyar-Rahim <rahimislam420.ri35@gmail.com>
2026-03-18 22:13:15 +0600

    jwt secret fix

--- backend/src/config/auth.config.js
+++ backend/src/config/auth.config.js
@@ -7,7 +7,7 @@ dotenv.config();
 // encode token
 const encodeToken = (email, id) => {
     const payload = { email, id };
-    const key = process.env.JWT_SECRET_KEY;
+    const key = process.env.JWT_SECRET;
     const expire = process.env.JWT_EXPIRES_IN;
 
     return jwt.sign(payload, key, { expiresIn: expire });
